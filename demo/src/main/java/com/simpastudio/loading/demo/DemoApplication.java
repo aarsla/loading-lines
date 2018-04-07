f65a629ecf4f09284eaabb189e90ec8815797bc3
Aid Arslanagic <aarsla@simpastudio.com>
2018-04-07 20:26:57 +0200

    Demo app

---
+++ demo/src/main/java/com/simpastudio/loading/demo/DemoApplication.java
@@ -0,0 +1,36 @@
+package com.simpastudio.loading.demo;
+
+import com.simpastudio.loading.demo.model.Message;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+
+@SpringBootApplication
+public class DemoApplication {
+
+    private static final Logger log = LoggerFactory.getLogger(DemoApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+
+    @Bean
+    public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+        return args -> {
+            Message message = restTemplate.getForObject(
+                    "http://localhost:9000/message", Message.class);
+            log.info(message.toString());
+        };
+    }
+}
